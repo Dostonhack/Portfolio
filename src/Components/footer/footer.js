b007e22a5d06b638ec4b4d8ff5c810863a0dfca4
Eliboyev Doston <doston1721@gmail.com>
2020-11-05 21:33:02 +0500

    0505

---
+++ src/Components/footer/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './footer.css'
+
+export default class footer extends Component {
+    render() {
+        return (
+            <div className='footerpage'>
+                <p>Eliboyev Doston  Web Full Stack Develpor  </p>
+                
+            </div>
+        )
+    }
+}
--- src/Components/skills/skills.js
+++ src/Components/skills/skills.js
@@ -11,7 +11,7 @@ export default class skills extends Component {
                 <div className='homeSkill'>
                     <div className='homeSkillin'>
                         <span>Dig a little deeper.</span>
-                        <a href="https://drive.google.com/file/d/1C1sf8hisoQratQoGGaEU3ZNE3Q5xWl_F/view?ths=true" download className='skillLink' target="_blank" download>DOWNLOAD RESUME </a>
+                        <a href="https://drive.google.com/file/d/1UtcO3vrplqySA-GPZ7XsGoF6DUSUzJcl/view?usp=sharing" download className='skillLink' target="_blank" download>DOWNLOAD RESUME </a>
                     </div>
                 </div>
                 <p className='myskills'>Technologies</p>
@@ -46,14 +46,7 @@ export default class skills extends Component {
                             <li>Npm </li>
                             <li>Version Control(Git)</li>
                             <li>WordPress (limited) </li>
-                            <li>Python (Basics)</li>
-                            <li>NumPy library (limited)</li>
-                            <li>SciPy library (limited)</li>
-                            <li>Pandas library (limited)</li>
-                            <li> Sikit-Learn library (limited)</li>
-                            <li>Matplotlib library (limited)</li>
-                            <li>Bokeh library (limited)</li>
-                            <li>Saborn library (limited)</li>
+                            
                         </ul>
                     
                     </div>
